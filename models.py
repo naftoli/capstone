ed6861c764d45fca4f05830136fcf62c97d62d76
naftoli rapoport <naftolir@gmail.cokm>
2023-05-18 18:36:21 -0400

    more changes to fix error

--- models.py
+++ models.py
@@ -12,6 +12,7 @@ def setup_db(app):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
+    db.create_all()
 
 ''' create a user model that includes first name, last name, email, and password '''
 class User(db.Model):
